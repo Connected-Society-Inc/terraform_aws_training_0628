ce02a869671335f213e54a996c0157c9ae81c529
garzoand <garzoand@gmail.com>
2021-06-28 07:50:29 -0700

    cm

---
+++ aws_env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# You should run this as follows: ". ./env.sh"
+export AWS_ACCESS_KEY_ID="anaccesskey"
+export AWS_SECRET_ACCESS_KEY="asecretkey"
+
+# On Windows:
+# $env:AWS_ACCESS_KEY_ID=
+# $env:AWS_SECRET_ACCESS_KEY=
+
