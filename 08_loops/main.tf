39c146341b877a8c3a1ac05fd5e4b11d806c0536
garzoand <garzoand@gmail.com>
2021-06-30 10:47:32 -0700

    cm

--- 08_loops/main.tf
+++ 08_loops/main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = "us-west-2"
 }
 
+variable "subnet_cidr_blocks" {
+    type = list(string)
+    default = ["10.1.1.0/24", /*"10.1.2.0/24",*/ "10.1.3.0/24"]
+}
+
 locals {
     vpc_cidr_block = "10.1.0.0/16"
-    subnet_cidr_blocks = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
 }
 
 resource "aws_vpc" "vpc" {
@@ -12,8 +16,17 @@ resource "aws_vpc" "vpc" {
 }
 
 // Task: create a subnet for each cidr block defined in the local.subnet_cidr_blocks list
-resource "aws_subnet" "subnet" {
-    count      = length(local.subnet_cidr_blocks)
+// "less perfect" solution is to use count, but beware of removing central elements from the list!
+/*resource "aws_subnet" "subnet" {
+    count      = length(var.subnet_cidr_blocks)
     vpc_id     = aws_vpc.vpc.id
-    // cidr_block = 
+    cidr_block = var.subnet_cidr_blocks[count.index]
+}*/
+
+resource "aws_subnet" "subnets" {
+    // other "loop" type:
+    for_each = toset(var.subnet_cidr_blocks)
+
+    vpc_id     = aws_vpc.vpc.id
+    cidr_block = each.value
 }
