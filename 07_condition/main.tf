613861c419a6f744b83d63eddf7975edc24e83bf
garzoand <garzoand@gmail.com>
2021-06-30 09:32:57 -0700

    cm

--- 07_condition/main.tf
+++ 07_condition/main.tf
@@ -3,11 +3,13 @@ provider "aws" {
 }
 
 locals {
-    create_instance = true # false
+    create_instance = true
 }
 
 // I just want to create this instance is local.create_instance == true
-resource "aws_instance" "ec2" {  
+resource "aws_instance" "ec2" {     
+    count = local.create_instance ? 1 : 0
+
     ami           = "ami-0721c9af7b9b75114"
     instance_type = "t3.micro"
 }
