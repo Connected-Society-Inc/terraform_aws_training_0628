af6110ae1a06d8cfaf1f1f45c4b2c513e191308f
garzoand <garzoand@gmail.com>
2021-07-01 11:05:36 -0700

    cm

--- 11_provisioner/main.tf
+++ 11_provisioner/main.tf
@@ -7,12 +7,69 @@ variable "vpc_id" {
     type = string
 }
 
+// aws_instance other_instance
+
 resource "aws_instance" "ec2" {
-    ami             = "ami-0721c9af7b9b75114"
-    instance_type   = "t3.micro"
-    key_name        = aws_key_pair.ssh_key.key_name
+
+    ami                    = "ami-0721c9af7b9b75114"
+    instance_type          = "t3.micro"
+    key_name               = aws_key_pair.ssh_key.key_name
     vpc_security_group_ids = [ aws_security_group.allow_ports.id ]
+
+    /* To install nginx, I need to run on the instance:
+       sudo amazon-linux-extras install -y nginx1
+       sudo systemctl start nginx
+       sudo systemctl enable nginx */
+    /*provisioner "remote-exec" {
+        inline = [
+            "sudo amazon-linux-extras install -y nginx1",
+            "sudo systemctl start nginx",
+            "sudo systemctl enable nginx"
+        ]
+        // Note: destroy time provisioners won't run on top of tainted resources
+        when = create # / destroy == will run when the resource will be destroyed        
+        connection {
+            type        = "ssh"
+            host        = self.public_ip
+            user        = "ec2-user"
+            private_key = file("./test_key")
+        }
+    }*/
+
+    provisioner "local-exec" {
+        command = "echo \"Hello World!\""
+        when = destroy
+    }   
+    // user_data = "yum install ..." <== This would be much better than using provisioners! (if you can)
 }
+
+resource "null_resource" "null" {
+
+    // null_resource won't be provisoned before the ec2 instance
+    depends_on = [ aws_instance.ec2 ]
+
+    // what change should trigger a re-creation event on top of this null_resource
+    triggers = {
+        script_content = file("./script.sh")
+    }
+
+    provisioner "remote-exec" {
+        script = "./script.sh"
+        connection {
+            type        = "ssh"
+            host        = aws_instance.ec2.public_ip
+            user        = "ec2-user"
+            private_key = file("./test_key")
+        }
+    }
+
+    provisioner "local-exec" {
+        command = "echo \"Hello from null resource provisioner!\""        
+    }   
+}
+
+
+// ========================================================================================
 
 resource "aws_key_pair" "ssh_key" {
     key_name = "ssh_key"
